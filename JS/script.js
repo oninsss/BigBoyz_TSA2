4b5daa480b0fc2db197f470771adff136221569f
oninsss <antoninobonagua993@gmail.com>
2024-02-01 03:40:20 +0800

    UPDATE JS AND ANIMATIONS

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,32 @@
-const rectOne = document.getElementById('_rectOne')
 const bigRectOne = document.getElementById('_bigRectOne')
 const bigRectTwo = document.getElementById('_bigRectTwo')
-const buttonOne = document.getElementById('_buttonOne')
-const buttonTwo = document.getElementById('_buttonTwo')
+const bigRectThree = document.getElementById('_bigRectThree')
+const bigRectFour = document.getElementById('_bigRectFour')
+const bigRectFive = document.getElementById('_bigRectFive')
+const bigRectSix = document.getElementById('_bigRectSix')
+const bigRectSeven = document.getElementById('_bigRectSeven')
+
+const cardOne = document.getElementById('_card1')
+const cardTwo = document.getElementById('_card2')
+const cardThree = document.getElementById('_card3')
+const cardFour = document.getElementById('_card4')
+const cardFive = document.getElementById('_card5')
+const cardSix = document.getElementById('_card6')
+const cardSeven = document.getElementById('_card7')
+
+const containerTwo = document.getElementById('_containerTwo')
 
 var rectMove = 0;
 var currentRect = "";
-var pastRect = currentRect;
+var pastRect = "";
+
+function pastRectAssigner(){
+    if (pastRect == currentRect){
+        pastRect = "";
+    }else{
+        pastRect = currentRect;
+    }
+}
 
 function moveRect(bigRectNum){
     bigRectNum.style.top = '20%';
@@ -18,26 +38,87 @@ function moveBackRect(bigRectNum){
 
 function moveBigRects(BrectMove){
     if (BrectMove == 1){
+        containerTwo.style.backgroundColor = '#80462b';
         moveRect(bigRectOne);
         moveBackRect(pastRect);
+        
     } else if (BrectMove == 2){
         moveRect(bigRectTwo);
         moveBackRect(pastRect);
-        pastRect = currentRect;
+    } else if (BrectMove == 3){
+        moveRect(bigRectThree);
+        moveBackRect(pastRect);
+    } else if (BrectMove == 4){
+        moveRect(bigRectFour);
+        moveBackRect(pastRect);
+    } else if (BrectMove == 5){
+        moveRect(bigRectFive);
+        moveBackRect(pastRect);
+    } else if (BrectMove == 6){
+        moveRect(bigRectSix);
+        moveBackRect(pastRect);
+    } else if (BrectMove == 7){
+        moveRect(bigRectSeven);
+        moveBackRect(pastRect);
     }
 }
 
-buttonOne.addEventListener('click', () => {
+cardOne.addEventListener('click', () => {
     rectMove = 1;
-    pastRect = currentRect; // Assign the value of currentRect to pastRect before moving the big rectangles.
+    pastRectAssigner();
     currentRect = bigRectOne;
     moveBigRects(rectMove);
 });
 
-buttonTwo.addEventListener('click', () => {
+cardTwo.addEventListener('click', () => {
     rectMove = 2;
-    pastRect = currentRect; // Assign the value of currentRect to pastRect before moving the big rectangles.
+    pastRectAssigner();
     currentRect = bigRectTwo;
     moveBigRects(rectMove);
 });
 
+cardThree.addEventListener('click', () => {
+    rectMove = 3;
+    pastRectAssigner();
+    currentRect = bigRectThree;
+    moveBigRects(rectMove);
+});
+
+cardFour.addEventListener('click', () => {
+    rectMove = 4;
+    pastRectAssigner();
+    currentRect = bigRectFour;
+    moveBigRects(rectMove);
+});
+
+cardFive.addEventListener('click', () => {
+    rectMove = 5;
+    pastRectAssigner();
+    currentRect = bigRectFive;
+    moveBigRects(rectMove);
+});
+
+cardSix.addEventListener('click', () => {
+    rectMove = 6;
+    pastRectAssigner();
+    currentRect = bigRectSix;
+    moveBigRects(rectMove);
+});
+
+cardSeven.addEventListener('click', () => {
+    rectMove = 7;
+    pastRectAssigner();
+    currentRect = bigRectSeven;
+    moveBigRects(rectMove);
+});
+
+cardOne.addEventListener('mouseover', () => {
+    cardOne.style.transition = 'background 0.5s ease'; // Add transition property
+    cardOne.style.background = 'url("https://i.pinimg.com/originals/d8/6f/3a/d86f3ab8192f5589eca93cd7725ad8e4.gif")';
+    cardOne.style.boxShadow = 'inset 50px 50px 100px #0d0d0d'
+});
+
+cardOne.addEventListener('mouseout', () => {
+    cardOne.style.background = "red";
+    cardOne.style.boxShadow = 'none'
+});
